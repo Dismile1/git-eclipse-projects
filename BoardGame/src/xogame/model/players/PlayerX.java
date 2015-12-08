03e1397561d2392e70b23d1350f265c62fa6d17a
Dmitry Tsurikov <awakeningtheseed@gmail.com>
2015-12-09 01:17:52 +0200

    PlayerX created

---
+++ BoardGame/src/xogame/model/players/PlayerX.java
@@ -0,0 +1,14 @@
+package xogame.model.players;
+
+import boardgame.model.Figure;
+import boardgame.model.Player;
+import xogame.model.figures.FigureX;
+
+public class PlayerX extends Player {
+	Figure figure;
+	
+	public PlayerX(final String name) {
+		super(name);
+		figure = new FigureX();
+	}
+}
