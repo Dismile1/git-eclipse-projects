5a85536f9ea3c0ec2bd3facb00d93d74f55b1abb
Dmitry Tsurikov <awakeningtheseed@gmail.com>
2015-12-08 22:16:58 +0200

    Interface IPlayer was changed to class Player

--- BoardGame/src/boardgame/model/IPlayer.java
+++
@@ -1,5 +0,0 @@
-package boardgame.model;
-
-public interface IPlayer {
-	public String getName();
-}
---
+++ BoardGame/src/boardgame/model/Player.java
@@ -0,0 +1,17 @@
+package boardgame.model;
+
+public class Player {
+	private String name;
+	
+	public Player(final String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(final String name) {
+		this.name = name;
+	}
+}
