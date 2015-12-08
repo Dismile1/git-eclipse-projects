b259b6ee0725251d4134562f34f5dfa14aec0f29
Dmitry Tsurikov <awakeningtheseed@gmail.com>
2015-12-08 22:13:06 +0200

    Refactored IBoard interface according to Figure changes

--- BoardGame/src/boardgame/model/IBoard.java
+++ BoardGame/src/boardgame/model/IBoard.java
@@ -3,7 +3,7 @@ package boardgame.model;
 public interface IBoard {
 	public int getHeight();
 	public int getWidth();
-	public IFigure getFigureAt(int rowNum, int colNum);
-	public void setFigureAt(int rowNum, int colNum, IFigure figure);
+	public Figure getFigureAt(int rowNum, int colNum);
+	public void setFigureAt(int rowNum, int colNum, Figure figure);
 	public boolean isFigureSet(int rowNum, int colNum);
 }
