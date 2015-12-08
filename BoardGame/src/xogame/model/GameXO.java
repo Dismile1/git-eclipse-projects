00bd8f8e8e3d1079d1df1e0e7b192136b74bc08b
Dmitry Tsurikov <awakeningtheseed@gmail.com>
2015-12-08 22:52:16 +0200

    Created model of XO Game

--- BoardGame/src/xogame/model/GameXO.java
+++ BoardGame/src/xogame/model/GameXO.java
@@ -1,12 +1,49 @@
 package xogame.model;
 
 import boardgame.model.IGame;
+import boardgame.model.Player;
+import xogame.model.figures.FigureO;
+import xogame.model.figures.FigureX;
 
 public class GameXO implements IGame {
-
+	private final String name;
+	private final BoardXO boardXO;
+	private final Player player1;
+	private final Player player2;
+	private final FigureO figureO;
+	private final FigureX figureX;
+	
+	public GameXO(final BoardXO boardXO, final Player player1, final Player player2) {
+		name = Constants.XO_GAME_NAME;
+		figureX = new FigureX();
+		figureO = new FigureO();
+		this.player1 = player1;
+		this.player2 = player2;
+		this.boardXO = boardXO;
+	}
+	
 	@Override
 	public String getName() {
-		return null;
+		return name;
+	}
+	
+	public FigureO getFigureO() {
+		return figureO;
+	}
+	
+	public FigureX getFigureX() {
+		return figureX;
+	}
+	
+	public Player getPlayer1() {
+		return player1;
+	}
+	
+	public Player getPlayer2() {
+		return player2;
 	}
 
+	public BoardXO getBoardXO() {
+		return boardXO;
+	}
 }
