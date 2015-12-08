5d85aa03ce7e7071858fa61eef5d0ed20a8cac08
Dmitry Tsurikov <awakeningtheseed@gmail.com>
2015-12-08 21:37:00 +0200

    Changed return value for setFigure method to void

--- BoardGame/src/boardgame/model/IBoard.java
+++ BoardGame/src/boardgame/model/IBoard.java
@@ -4,6 +4,6 @@ public interface IBoard {
 	public int getHeight();
 	public int getWidth();
 	public IFigure getFigureAt(int rowNum, int colNum);
-	public boolean setFigureAt(int rowNum, int colNum, IFigure figure);
+	public void setFigureAt(int rowNum, int colNum, IFigure figure);
 	public boolean isFigureSet(int rowNum, int colNum);
 }
