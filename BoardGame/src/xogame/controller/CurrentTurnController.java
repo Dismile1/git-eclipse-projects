b90108f6ac33eb1799652aeb8fff8ed0ac4563a1
Dmitry Tsurikov <awakeningtheseed@gmail.com>
2015-12-09 15:08:11 +0200

    Created CurrentTurnController

---
+++ BoardGame/src/xogame/controller/CurrentTurnController.java
@@ -0,0 +1,11 @@
+package xogame.controller;
+
+public class CurrentTurnController {
+	private int movesCounter = 0;
+	
+	
+	
+	public CurrentTurnController() {
+		// TODO Auto-generated constructor stub
+	}
+}
